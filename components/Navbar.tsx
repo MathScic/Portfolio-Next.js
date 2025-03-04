704a4fd4bd0a38c580a734d3b1442757077a732f
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-03-04 13:08:17 +0100

    Ajout anim burger icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,14 @@ import { IoPersonSharp } from "react-icons/io5";
 import { FaLaptopCode } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross1 } from "react-icons/rx";
 import { useState } from "react";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="w-full p-4 bg-[#f5f5f5] flex items-center px-0  ">
+    <nav className="w-full p-4 bg-[#f5f5f5] flex items-center px-0">
       <div className="w-[100%] sm:w-[70%] pl-2 pr-2 mx-auto flex justify-between items-center text-[#333]">
         {/* Logo et H1 alignés à gauche */}
         <Link href="/">
@@ -27,18 +28,58 @@ const Navbar = () => {
 
         {/* Navigation Menu (affichage mobile) */}
         <div className="sm:hidden flex items-center w-full justify-between pl-4 pr-4">
-          {/* Le burger icon collé à droite avec un padding */}
-          <button onClick={() => setMenuOpen(!menuOpen)} className="ml-auto">
-            <GiHamburgerMenu className="w-6 h-6" />
+          {/* Le burger icon se transforme en croix lorsque le menu est ouvert */}
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="ml-auto relative z-50"
+          >
+            {menuOpen ? (
+              <RxCross1 className="w-6 h-6 transition-transform duration-300" />
+            ) : (
+              <GiHamburgerMenu className="w-6 h-6 transition-transform duration-300" />
+            )}
           </button>
         </div>
 
-        {/* Menu Desktop (affichage grand écran) */}
-        <ul
+        {/* Menu Mobile */}
+        <div
           className={`${
-            menuOpen ? "block" : "hidden"
-          } sm:flex sm:space-x-8 absolute sm:static top-16 left-0 w-full sm:w-auto bg-[#333] sm:bg-transparent transition-all ease-in-out duration-300 p-6 sm:p-0`}
+            menuOpen ? "translate-x-0" : "translate-x-full"
+          } sm:hidden absolute top-16 right-0 w-full bg-[#333] p-6 transition-all duration-500 ease-in-out transform`}
         >
+          <ul className="space-y-6 text-white">
+            <li>
+              <Link
+                href="/about"
+                className="flex items-center text-[#ff7a29] hover:text-[#ff7a29] transition duration-300"
+              >
+                <IoPersonSharp className="mr-2" />
+                <span>A propos</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/projects"
+                className="flex items-center text-[#ff7a29] hover:text-[#ff7a29] transition duration-300"
+              >
+                <FaLaptopCode className="mr-2" />
+                <span>Projets</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="flex items-center text-[#ff7a29] hover:text-[#ff7a29] transition duration-300"
+              >
+                <IoIosMail className="mr-2" />
+                <span>Contact</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Menu Desktop */}
+        <ul className="sm:flex sm:space-x-8 sm:flex-row sm:static sm:bg-transparent hidden">
           <li>
             <Link
               href="/about"
