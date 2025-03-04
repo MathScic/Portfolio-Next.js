f75230779e8e615664c667a4f8cc1e37948deb4f
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-03-04 13:31:42 +0100

    modif pour vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Active le mode strict pour React
+  swcMinify: true, // Optimise la taille du code
 };
 
 export default nextConfig;
